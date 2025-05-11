6e39d0892101efcb7325ee76dac6e2c7ac6460e6
Divy Srivastava <namansrivastava21sep2008@gmail.com>
2025-05-11 17:04:17 +0530

    Completed the exercise

--- 05_joinStrings/joinStrings.spec.js
+++ 05_joinStrings/joinStrings.spec.js
@@ -1,13 +1,13 @@
 const values = require('./joinStrings')
 
 describe('step 2', () => {
-  test('firstName is Carlos', () => {
+  test.skip('firstName is Carlos', () => {
     expect(values.firstName).toEqual('Carlos');
   });
   test.skip('lastName is Stevenson', () => {
     expect(values.lastName).toEqual('Stevenson');
   });
-  test.skip('thisYear is 1965', () => {
+  test('thisYear is 1965', () => {
     expect(values.thisYear).toEqual(1965);
   });
   test.skip('birthYear is 1947', () => {
@@ -22,7 +22,7 @@ describe('step 3', () => {
   test.skip('fullName is Carlos Stevenson', () => {
     expect(values.fullName).toEqual('Carlos Stevenson');
   });
-  test.skip('age is 18', () => {
+  test('age is 18', () => {
     expect(values.age).toEqual(18);
   });
 });
