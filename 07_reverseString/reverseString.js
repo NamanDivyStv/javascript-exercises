349483133394dd0f552a3b5dc88407bd69f0009c
Divy Srivastava <namansrivastava21sep2008@gmail.com>
2025-05-11 17:05:25 +0530

    Completed the exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+     let len=str.length;
+     let newstr="";
+     for(let i=len-1;i>=0;i--){
+         newstr=newstr+str.charAt(i);
+     }
+     return(newstr);
 };
 
 // Do not edit below this line
