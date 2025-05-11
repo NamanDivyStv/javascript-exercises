e3d259f44258955a1158fe9f5784099757b84ca0
Divy Srivastava <namansrivastava21sep2008@gmail.com>
2025-05-11 17:06:20 +0530

    Completed the exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(start,end) {
+    if(start<0||end<0){
+        return("ERROR");
+    }
+    if(((start*10)%10)!=0||((end*10)%10)!=0){
+        return("ERROR");
+    }
+    if(typeof(start)!="number"||typeof(end)!="number"){
+        return("ERROR");
+    }
+    if(start<end){
+        let i;
+        let sum=0;
+        for(i=start;i<=end;i++){
+           sum=sum+i;
+        }
+        return(sum);
+    }
+    else if(start>end){
+        let i;
+        let sum=0;
+        for(i=end;i<=start;i++){
+           sum=sum+i;
+        }
+        return(sum);
+    }
+    
 };
 
 // Do not edit below this line
